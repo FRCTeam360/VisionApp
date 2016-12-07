c447862a2ade0de715cba03d059df081cdb9fb4e
poshamazing <poshamazing@gmail.com>
2016-12-06 16:10:17 -0800

    added the vision app code

---
+++ TestVision/app/src/main/java/com/example/user/testvision/Vision.java
@@ -0,0 +1,155 @@
+package com.example.user.testvision;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.*;
+import org.opencv.imgproc.*;
+import org.opencv.*;
+
+
+import java.util.ArrayList;
+
+// OpenCV Classes
+
+public class Vision extends AppCompatActivity implements CvCameraViewListener2 {
+
+    // Used for logging success or failure messages
+    private static final String TAG = "OCVSample::Activity";
+
+    // Loads camera view of OpenCV for us to use. This lets us see using OpenCV
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    // Used in Camera selection from menu (when implemented)
+    private boolean              mIsJavaCamera = true;
+    private MenuItem             mItemSwitchCamera = null;
+    ArrayList mCountours;
+    // These variables are used (at the moment) to fix camera orientation from 270degree to 0degree
+    Mat mRgba;
+    Mat mRgbaF;
+    Mat mRgbaT;
+    Mat mHSV;
+    Mat mThresholded;
+    Mat mHeir;
+    Scalar mLowFilter, mHighFilter, mContourColor;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    public Vision() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_vision);
+
+        mOpenCvCameraView = (JavaCameraView) findViewById(R.id.show_camera_activity_java_surface_view);
+
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaT = new Mat(width, width, CvType.CV_8UC4);
+        mHSV = new Mat(width, width, CvType.CV_8UC4);
+        mThresholded = new Mat(width, width, CvType.CV_8UC4);
+        mHeir = new Mat(width, width, CvType.CV_8UC4);
+        mLowFilter = new Scalar(0, 0, 0);
+        mHighFilter = new Scalar(110, 255, 185);
+        mContourColor = new Scalar(124, 124, 124);
+        mCountours = new ArrayList();
+    }
+
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+
+        // TODO Auto-generated method stub
+        mRgba = inputFrame.rgba();
+        // Rotate mRgba 90 degrees
+        mCountours.clear();
+        Core.transpose(mRgba, mRgbaT);
+        Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0,0, 0);
+        Imgproc.cvtColor(mRgba, mHSV, Imgproc.COLOR_RGB2HSV);
+        Core.inRange(mHSV, mLowFilter, mHighFilter, mThresholded);
+        Imgproc.findContours(mThresholded, mCountours, mHeir, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        for(int i = 0; i < mCountours.size(); i++){
+            Rect rec = Imgproc.boundingRect((MatOfPoint) mCountours.get(i));
+            if(rec.width > 40 && rec.height > 40) {
+                Imgproc.drawContours(mThresholded, mCountours, i, mContourColor);
+            }
+        }
+
+        //Core.flip(mRgbaF, mRgba, 1 );
+
+        return mThresholded; // This function must return
+    }
+}
