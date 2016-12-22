ea30add4509e766055d66ad799e5d22532f95851
jGrac <poshamazing@gmail.com>
2016-12-21 16:47:48 -0800

    added VisionMessage class to get target info

---
+++ SimulatedRobotComm/SimulatedRobotServer/src/org/usfirst/frc/team360/robot/VisionMessage.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team360.robot;
+
+public class VisionMessage {
+
+	double centerYCoor;
+	double centerXCoor;
+	double distance;
+	double angle;
+	long timeCaptured;
+	
+	public VisionMessage(){
+		
+	}
+
+}
